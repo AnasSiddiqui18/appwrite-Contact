4b3bc314826e34f19a9876bf98aaa3d987acf5fc
Anas Siddiqui <anassiddiqui0910@gmail.com>
2023-11-30 05:02:24 +0500

    Changes Made

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -4,14 +4,27 @@ import conf from "../conf/conf";
 import { useLocation } from "react-router-dom";
 import { Query } from "appwrite";
 import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
 
 const Profile = () => {
   const [contactlist, setContactList] = useState([]);
   const [updatemode, setUpdateMode] = useState(false);
   const [documentId, setDocumentId] = useState("");
+
   const location = useLocation();
 
   const response = location && location.state && location.state.userId;
+
+  const schema = yup.object().shape({
+    name: yup
+      .string("Should be in string")
+      .min(4, "Name must be atleast 4 characters long"),
+    number: yup
+      .string("Should be in string")
+      .max(15, "Maximum 15 characters is allowed for the number")
+      .min(7, "number must be atleast 7 characters long"),
+  });
 
   const {
     register,
@@ -19,7 +32,9 @@ const Profile = () => {
     reset,
     setValue,
     formState: { errors },
-  } = useForm();
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
 
   const onSubmit = (data) => {
     if (updatemode) {
@@ -106,6 +121,8 @@ const Profile = () => {
     }
   };
 
+  const reversedItems = contactlist.slice().reverse();
+
   return (
     <div className="h-[calc(100vh-48px)] bg-gray-400 flex flex-col items-center justify-center">
       <div className="w-[500px] h-[400px] bg-orange-200 rounded-lg px-4 py-4 mt-[70px] flex gap-2 flex-col">
@@ -113,31 +130,20 @@ const Profile = () => {
           onSubmit={handleSubmit(onSubmit)}
           className="flex gap-y-3 flex-col"
         >
-          {errors.name && <p className="text-red-500">{errors.name.message}</p>}
+          <p className="text-red-500">{errors.name?.message}</p>
           <input
-            {...register("name", {
-              required: "Name Is Required!",
-            })}
+            {...register("name")}
             type="text"
-            name="name"
             className="outline-none px-3 py-1 rounded-md w-full"
             placeholder="Enter Name Here"
           />
-
-          {errors.number && (
-            <p className="text-red-500">{errors.number.message}</p>
-          )}
-
+          <p className="text-red-500">{errors.number?.message}</p>
           <input
-            {...register("number", {
-              required: "Number Is Required!",
-            })}
-            type="text"
-            name="number"
-            className="outline-none px-3 py-1 rounded-md w-full"
+            {...register("number")}
+            type="number"
+            className="outline-none px-3 py-1 rounded-md w-full "
             placeholder="Enter Contact Number"
           />
-
           <span className="flex justify-between mt-2">
             {!updatemode ? (
               <button
@@ -157,7 +163,7 @@ const Profile = () => {
           </span>
         </form>
         <div className="overflow-auto flex flex-col w-full items-center gap-3">
-          {contactlist.map((data, i) => (
+          {reversedItems.map((data, i) => (
             <div key={i} className="flex items-center w-full gap-2 ">
               <div className="bg-gray-200 px-3 py-2 rounded-md w-full mr-2">
                 <span className="font-semibold uppercase">{data.name}</span>
@@ -165,21 +171,22 @@ const Profile = () => {
                   {data.number}
                   <span className="flex gap-3">
                     <button
-                      className="cursor-pointer bg-[crimson] text-white px-2 rounded-md"
                       onClick={() => {
                         setValue("name", data.name);
                         setValue("number", data.number);
                         setUpdateMode(true);
                         setDocumentId(data.$id);
                       }}
+                      className="cursor-pointer bg-[crimson] text-white px-2 rounded-md"
                     >
                       Update
                     </button>
                     <button
-                      onClick={() => {
+                      onClick={(event) => {
                         deleteDocument(data.$id);
+                        event.target.disabled = "true";
                       }}
-                      className="cursor-pointer bg-orange-600 text-white px-2 rounded-md"
+                      className="cursor-pointer bg-orange-600 disabled:opacity-60 disabled:pointer-events-none text-white px-2 rounded-md"
                     >
                       Delete
                     </button>
