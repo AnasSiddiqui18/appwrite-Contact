839446d2e20e7b6e13d479038d54cd2bc6544a70
Anas Siddiqui <anassiddiqui0910@gmail.com>
2023-12-19 06:17:15 +0500

    Changes made in the OAuth

--- src/context/userContext.jsx
+++ src/context/userContext.jsx
@@ -140,26 +140,10 @@ export const AuthProvider = ({ children }) => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  const getURL = () => {
-    let url =
-      import.meta.env.VITE_NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
-      import.meta.env.VITE_NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
-      "http://localhost:5173/";
-    // Make sure to include `https://` when not localhost.
-    url = url.includes("http") ? url : `https://${url}`;
-    // Make sure to include a trailing `/`.
-    url = url.charAt(url.length - 1) === "/" ? url : `${url}/`;
-    return url;
-  };
-
   const googleLogin = async () => {
     try {
       const { data, error } = await supabase.auth.signInWithOAuth({
         provider: "google",
-
-        options: {
-          redirectTo: getURL(),
-        },
       });
 
       console.log(`login with google`, data);
