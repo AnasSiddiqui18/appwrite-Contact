e784b8bc736365204fe52113de11a15c014d4d33
Anas Siddiqui <anassiddiqui0910@gmail.com>
2023-12-19 06:09:47 +0500

    Fixed: OAuth login

--- src/context/userContext.jsx
+++ src/context/userContext.jsx
@@ -142,7 +142,7 @@ export const AuthProvider = ({ children }) => {
 
   const getURL = () => {
     let url =
-      import.meta?.env.VITE_SUPABASE_PROJECT_URL ?? // Set this to your site URL in production env.
+      import.meta.env.VITE_SUPABASE_PROJECT_URL ?? // Set this to your site URL in production env.
       import.meta.env.VITE_NEXT_PUBLIC_SITE_URL ?? // Automatically set by Vercel.
       "http://localhost:5173/";
     // Make sure to include `https://` when not localhost.
